7be6b16522c525c0647d82756a5ada38c5167f36
Federico Urbani <urbanifederico5@gmail.com>
2023-01-25 13:30:00 +0100

    updated main

---
+++ OtherOperatorss/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 15;
+        a +=5;
+        a-=4;
+        int b=a+1;
+
+            System.out.println("at the start of this program b is equal to:"+b);
+        if (b%2==0){
+            System.out.println("b is odd");
+        }else{
+            System.out.println("b is not odd");
+        }
+        if(b*b+1%3==0){
+            System.out.println("the result is a multiple of 3");
+        }else{
+            System.out.println("the result is not a multiple of 3");
+        }
+    }
+}
